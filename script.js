ec70ff7a267a222ee79f45cd19d79d83d4c55a6f
danaradi07 <127088731+danardi07@users.noreply.github.com>
2024-04-07 23:06:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+video.addEventListener("Click", function video(){
+  alert("video cinematicnya")
+});
+
+audio.addEventListener("Click", function audio(){
+  alert("Backsound cinematicnya")
+});
+
+image.addEventListener("Click", function image(){
+  alert("Foto Mobilnya")
+});
+
